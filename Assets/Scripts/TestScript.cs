2f1d39b4acb6486a8ee402e96a317abaeb7bc402
Tiago Stockler <tiagodstockler@gmail.com>
2018-11-28 19:11:14 -0200

    SDKs added: DevToDev, Game Analytics, Appodeal and Google Play Games. Project is building.

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using AppodealAds.Unity.Api;
+using AppodealAds.Unity.Common;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
